4018bd1af04f23202a26728f89bad0db113dc9b1
MrTinker64 <avi.micah@gmail.com>
2023-11-21 07:23:47 -0800

    made a serial input reader

---
+++ serialReader/serialReader.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(2, INPUT);
+}
+
+void loop()
+{
+    int sensorValue = digitalRead(2);
+    Serial.println(sensorValue);
+}
